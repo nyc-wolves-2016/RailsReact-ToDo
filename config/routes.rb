848c37e46e706729ed88d0b7579afcd55fc5fed1
neelgupta2 <neelgupta2@gmail.com>
2016-10-09 16:30:37 -0400

    Add task controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  # Return new task form
+  get '/users/:user_id/lists/:list_id/tasks/new'
+
+  # Post the new task under a specific list
+  post '/users/:user_id/lists/:list_id/tasks/new'
+
+  # Probably won't use
+  ## get 'tasks/show'
+
+  
+  get 'tasks/destroy'
+
   get 'lists/index'
 
   get 'lists/new'
